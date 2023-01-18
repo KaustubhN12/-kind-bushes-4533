1d378c50132c9a9c26ad1021fe0236c93aeaf404
Shantanu Nawade <nawadekaustubh@gmail.com>
2023-01-18 22:06:17 +0530

    day2

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# -kind-bushes-4533
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Nav from "./Components/Navbar";
+import Carousel from "./Components/carousel1";
+import LargeWithNewsletter from "./Components/Footer"
+function App() {
+  return (
+    <>
+    <Nav/>
+    <Carousel/>
+    <LargeWithNewsletter/>
+    </>
+  );
+}
+
+export default App;
